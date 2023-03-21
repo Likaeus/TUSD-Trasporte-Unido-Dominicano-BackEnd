31e3b341ae5a9cb1d4046fcd1ed7e5df17f62e38
Likaeus <rodriguezvarona@gmail.com>
2023-03-21 13:46:57 -0400

    deleted database connection string

--- Models/tusddatabaseContext.cs
+++ Models/tusddatabaseContext.cs
@@ -29,7 +29,7 @@ namespace TUSDWebApi.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=tcp:tusd-server.database.windows.net,1433;Initial Catalog=tusd-database;Persist Security Info=False;User ID=tusd-admin;Password=Hola1234;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+                optionsBuilder.UseSqlServer("");
             }
         }
 
